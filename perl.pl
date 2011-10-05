32422aca391720e348c8edc769b7a27ce223fbb6
Nicolae Dicu <frdct_nic@yahoo.com>
2011-10-05 19:04:58 +0300

    update2

--- perl.pl
+++ perl.pl
@@ -1,8 +1,17 @@
-print "Hello World.\n";
-print "Bye";
-#comm
-=pod
-aasd
+$a = 50;
+$b = "mesaj";
+$c = 4.5;
 
-dasd
-=cut
+print "a=$a,b=$b,c=$c\n";
+print 'a=$a\n';
+
+@vector=(3,"asda",4,5);
+print $vector[1];
+print $vector[-1];
+print scalar @vector;
+
+%hash = ("Luni" => 1, "Marti" => 2, "MIercuri" =>3);
+print $hash{"Marti"};
+print keys%hash;
+print values%hash;
+
